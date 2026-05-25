da815d5db39fc5dda5328563a7f710e64af2d5c3
Santiago Gutiérrez <157374921+santiagoguti17@users.noreply.github.com>
2026-05-25 17:30:38 -0500

    Update participant list for tournament: change SANTI to JUAN

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from tournament import run_tournament, play
 # Read all files within subfolder of "groups"
 participants = find_importable_classes("groups", Policy)
 
-# Build a participant list (name, class) for GUTI vs SANTI
+# Build a participant list (name, class) for GUTI vs JUAN
 players = [
     ("Group B GUTI", participants["Group B GUTI"]),
-    ("Group C SANTI", participants["Group C SANTI"]),
+    ("Group A JUAN", participants["Group A JUAN"]),
 ]
 
 # Run the tournament
